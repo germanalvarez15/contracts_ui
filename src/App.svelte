b746b97a7e6c4acfaf1d4e629fe75b53494a12b4
German Alvarez <germanalvarez15@gmail.com>
2022-05-04 15:26:52 -0300

    change loader

--- src/App.svelte
+++ src/App.svelte
@@ -33,8 +33,7 @@ import Header from "./components/HeaderContainer.svelte";
 <main>
 	{#await contratosPromise}
 		<div class="loader-container">
-			<img src="https://media.giphy.com/media/tXL4FHPSnVJ0A/giphy.gif" alt="Waiting"/>
-			<p>Please be patient while we collect all the data</p>
+			<div class="loader"></div>
 		</div>
 	{:then data}
 		<Header on:message={handleMessage}></Header>
@@ -75,5 +74,18 @@ import Header from "./components/HeaderContainer.svelte";
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
+	}
+	.loader {
+		border: 16px solid #f3f3f3; /* Light grey */
+		border-top: 16px solid #3498db; /* Blue */
+		border-radius: 50%;
+		width: 120px;
+		height: 120px;
+		animation: spin 2s linear infinite;
+	}
+
+	@keyframes spin {
+		0% { transform: rotate(0deg); }
+		100% { transform: rotate(360deg); }
 	}
 </style>
